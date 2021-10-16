291b932e43664d860e240b024020fe0e89bd10ff
Matthew Mercuri <mercurimatthew@gmail.com>
2021-10-16 12:02:49 -0400

    initial commit

---
+++ backend/data.py
@@ -0,0 +1,12 @@
+import requests
+
+"""
+API DOCUMENTATION:
+https://gitlab.com/dword4/nhlapi/-/tree/master
+"""
+
+BASE_URL = "https://statsapi.web.nhl.com/api/v1/"
+
+
+def get_schedule():
+    pass
