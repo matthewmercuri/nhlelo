ddd7feb4d9ad06d1fb2b198c9614eea36f96e8e3
Matthew Mercuri <mercurimatthew@gmail.com>
2021-10-16 16:59:25 -0400

    implemented home ice and b2b adj

--- backend/elo.py
+++ backend/elo.py
@@ -5,14 +5,32 @@ FINDING FACTORS:
 """
 
 K_FACTOR = 16
-HOME_ADJ = 3.3
-B2B_ADJ = 5
+HOME_ADJ = 0.033
+B2B_ADJ = 0.10
 
 
 def elo_calculator(
-    away_team_elo: int, home_team_elo: int, team_map: dict = None
-) -> tuple[int, int]:
+    away_team_elo: int,
+    home_team_elo: int,
+    away_b2b: int,
+    home_b2b: int,
+    team_map: dict = None,
+) -> tuple[float, float]:
     expected_score_away = 1 / (1 + (10 ** ((home_team_elo - away_team_elo) / 400)))
+
+    # adjusting for home ice advantage
+    expected_score_away -= HOME_ADJ
+
+    # adjusting for back-to-back
+    if (away_b2b == 1) and (home_b2b != 1):
+        expected_score_away -= B2B_ADJ
+    elif (home_b2b == 1) and (away_b2b != 1):
+        expected_score_away += B2B_ADJ
+
     expected_score_home = 1 - expected_score_away
 
-    return away_team_elo, home_team_elo
+    return expected_score_away, expected_score_home
+
+
+# def update_elo(away_score: int, home_score: int) -> tuple[int, int]:
+#     return away_team_elo, home_team_elo
