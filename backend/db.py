166594da78f3c4bcf2ae0e1b98f7169b5a41f1cd
Matthew Mercuri <mercurimatthew@gmail.com>
2021-11-27 13:30:13 -0500

    implemented caching system for games data

--- backend/db.py
+++ backend/db.py
@@ -7,5 +7,4 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 
 client = MongoClient(MONGO_URI)
-
-print(client.database_names())
+db = client["nhlelo"]
--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,35 @@
+from datetime import date, timedelta
 from fastapi import FastAPI
 import operator
 
+from db import db
 from elo_system import EloSystem
 
 app = FastAPI()
+
+
+"""
+TODO:
+- implement caching system
+- make updates to db by date
+- first retrieve res in db
+- see if the date is valid
+- regen if not
+"""
+
+
+def _get_recent_games(elo_df):
+    today = str(date.today())
+    yesterday = str(date.today() - timedelta(days=1))
+    tomorrow = str(date.today() + timedelta(days=1))
+    acceptable_dates = [yesterday, today, tomorrow]
+
+    for index, game in elo_df.copy().items():
+        game_date = game["Date"].strftime("%Y-%m-%d")
+        if game_date not in acceptable_dates:
+            del elo_df[index]
+
+    return elo_df
 
 
 @app.get("/")
@@ -13,8 +39,17 @@ async def root():
 
 @app.get("/elotable")
 def elo_table():
-    elo_df = EloSystem.process_elo_df()
-    return elo_df.to_dict(orient="index")
+    elo_df = db.elo_table.find_one({"date_generated": str(date.today())})
+
+    if elo_df is None:
+        elo_df = EloSystem.process_elo_df().to_dict(orient="index")
+        elo_df = _get_recent_games(elo_df)
+        elo_df["date_generated"] = str(date.today())
+        db.elo_table.insert_one(elo_df)
+
+    elo_df["id"] = str(elo_df.pop("_id"))  # pydantic needs this line
+
+    return elo_df
 
 
 @app.get("/teamelos")
