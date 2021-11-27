4d2686b950eed9fe455ac725a1df584ebc072b94
Matthew Mercuri <mercurimatthew@gmail.com>
2021-11-27 11:44:45 -0500

    implemented mongo connection

---
+++ backend/db.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+from pymongo import MongoClient
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = MongoClient(MONGO_URI)
+
+print(client.database_names())
