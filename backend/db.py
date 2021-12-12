c76fe35b35ad5e86eb938ff337566846e8a06dc9
Matthew Mercuri <mercurimatthew@gmail.com>
2021-12-12 12:33:46 -0500

    better logging

--- backend/db.py
+++ backend/db.py
@@ -3,6 +3,10 @@ import logging
 import os
 from pymongo import MongoClient
 
+logging.debug("===== TRYING TO LOAD ENV VARS =====")
+logging.debug(f"ENV VARS === {os.environ}")
+print("===== THIS IS ONLY A TEST =====")
+
 try:
     load_dotenv()
 
