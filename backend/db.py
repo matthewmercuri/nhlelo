f8749655d69f38339d0a55c69facbac261cdd5a8
Matthew Mercuri <mercurimatthew@gmail.com>
2021-12-12 12:48:51 -0500

    logging cleanup

--- backend/db.py
+++ backend/db.py
@@ -1,11 +1,6 @@
 from dotenv import load_dotenv
-import logging
 import os
 from pymongo import MongoClient
-
-print("===== TRYING TO LOAD ENV VARS =====")
-print(f"ENV VARS === {os.environ}")
-print("===== THIS IS ONLY A TEST =====")
 
 try:
     load_dotenv()
@@ -15,4 +10,4 @@ try:
     client = MongoClient(MONGO_URI)
     db = client["nhlelo"]
 except:
-    logging.debug("error loading env variable")
+    print("error loading env variable")
