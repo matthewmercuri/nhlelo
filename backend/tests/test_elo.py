b34ced75f3322efd760fec290cf7c69f96360aed
Matthew Mercuri <mercurimatthew@gmail.com>
2021-12-14 19:37:30 -0500

    added tests for elo_calculator

--- backend/tests/test_elo.py
+++ backend/tests/test_elo.py
@@ -7,4 +7,18 @@ Command: python -m pytest tests/
 
 
 def test_elo_calculator():
-    pass
+    expected_score_away, expected_score_home = elo_calculator(1500, 1500, 0, 0)
+    assert round(expected_score_away, 3) == 0.467
+    assert round(expected_score_home, 3) == 0.533
+
+    expected_score_away, expected_score_home = elo_calculator(1500, 1500, 1, 0)
+    assert round(expected_score_away, 3) == 0.367
+    assert round(expected_score_home, 3) == 0.633
+
+    expected_score_away, expected_score_home = elo_calculator(1400, 1600, 0, 0)
+    assert round(expected_score_away, 3) == 0.207
+    assert round(expected_score_home, 3) == 0.793
+
+    expected_score_away, expected_score_home = elo_calculator(1600, 1400, 0, 0)
+    assert round(expected_score_away, 3) == 0.727
+    assert round(expected_score_home, 3) == 0.273
