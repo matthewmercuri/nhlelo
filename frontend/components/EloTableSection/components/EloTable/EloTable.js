cc172c586021c2d61e666bf62af44d50a569bb76
Matthew Mercuri <mercurimatthew@gmail.com>
2022-01-15 13:43:54 -0500

    main elo ranking table logic complete

--- frontend/components/EloTableSection/components/EloTable/EloTable.js
+++ frontend/components/EloTableSection/components/EloTable/EloTable.js
@@ -1,22 +1,53 @@
-import { useRef, useState } from "react"
-const axios = require("axios").default
-import { BASE_URL } from "../../../../services/webapi"
+import { useState } from "react"
+import useSWR from "swr"
+import { BASE_URL, fetcher } from "../../../../services/webapi"
 import styles from './EloTable.module.css'
 
 export default function EloTable() {
-  const [isLoading, setIsLoading] = useState(true)
-  const [hasErrors, setHasErrors] = useState(false)
-  const [tableData, setTableData] = useState(null)
+  const { data, error } = useSWR(`${BASE_URL}/teamelotable`, fetcher)
+  const [showAll, setShowAll] = useState(false)
+  const isLoading = !data && !error
 
-  axios.get(`${BASE_URL}/teamelotable`).then(({ data }) => {
-    setIsLoading(false)
-    setTableData(data.data)
-  }).catch(error => {
-    setIsLoading(false)
-    setHasErrors(true)
-  })
+  console.log(data)
+  console.log(isLoading)
+  console.log(showAll)
 
   return (
-    <div className={styles.EloTable} />
+    <div className={styles.eloTableContainer}>
+      <table className={styles.eloTable} cellSpacing={0}>
+        <tr>
+          <th className={styles.endCol}>position</th>
+          <th>team</th>
+          <th className={styles.endCol}>elo</th>
+        </tr>
+        {data && data.data && (
+          data.data.map((row, index) => {
+            const teamName = Object.keys(row)[0]
+            const teamElo = Math.round(row[teamName])
+            const teamRank = index + 1
+            const eloStyle = teamElo >= 1500 ? styles.eloIndicatorGood : styles.eloIndicatorBad
+
+            if (teamRank > 10 && !showAll) return null
+
+            return (
+              <tr key={teamName}>
+                <td>{teamRank}</td>
+                <td>{teamName}</td>
+                <td>
+                  <div className={styles.eloIndicatorContainer}>
+                    <div className={eloStyle}>{teamElo}</div>
+                  </div>
+                </td>
+              </tr>
+            )
+          })
+        )}
+      </table>
+      {data && data.data && (
+        <div onClick={() => setShowAll(!showAll)} className={styles.showAllButton}>
+          {!showAll ? "show all" : "collapse all"}
+        </div>
+      )}
+    </div>
   )
 }
--- frontend/services/webapi.js
+++ frontend/services/webapi.js
@@ -1,1 +1,5 @@
+import axios from "axios"
+
+export const fetcher = url => axios.get(url).then(res => res.data)
+
 export const BASE_URL = "https://nhl-elo-api-app.herokuapp.com"
