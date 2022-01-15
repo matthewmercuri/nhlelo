dafc6ef50a7e5bfc68b87b5d17296d158fb4b363
Matthew Mercuri <mercurimatthew@gmail.com>
2022-01-15 09:34:33 -0500

    verified caching; added logging; cleanup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,6 @@
 from datetime import date, timedelta
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import operator
 
 from app.db import db
 from app.data import Data
@@ -33,10 +32,13 @@ def elo_table():
     elo_df = db.elo_table.find_one({"date_generated": str(date.today())})
 
     if elo_df is None:
+        print("Calculating elos (cache invalid) -- /eloschedule")
         DataGrabber = Data()
         elo_df = DataGrabber.process_schedule_df().to_dict(orient="index")
         elo_df["date_generated"] = str(date.today())
         db.elo_table.insert_one(elo_df)
+    else:
+        print("Returning cached result -- /eloschedule")
 
     elo_df["id"] = str(elo_df.pop("_id"))  # pydantic needs this line
     date_generated = elo_df.pop("date_generated")
@@ -55,10 +57,13 @@ def team_elos():
     team_elo_dict = db.team_elos.find_one({"date_generated": str(date.today())})
 
     if team_elo_dict is None:
+        print("Calculating elos (cache invalid) -- /teamelotable")
         DataGrabber = Data()
         team_elo_dict = DataGrabber.get_team_elos().to_dict()[0]
         team_elo_dict["date_generated"] = str(date.today())
         db.team_elos.insert_one(team_elo_dict)
+    else:
+        print("Returning cached result -- /teamelotable")
 
     team_elo_dict["id"] = str(team_elo_dict.pop("_id"))  # pydantic needs this line
     date_generated = team_elo_dict.pop("date_generated")
