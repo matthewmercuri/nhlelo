38696cdda3eb5a1d56fae2a6cc02ee4689171d22
Matthew Mercuri <mercurimatthew@gmail.com>
2022-02-07 19:12:54 -0500

    fixed error where not properly showing away b2b

--- frontend/components/GameCardSection/components/GameCard/GameCard.js
+++ frontend/components/GameCardSection/components/GameCard/GameCard.js
@@ -15,7 +15,7 @@ export default function GameCard({ gameData }) {
           <p className={`${styles.teamElo} ${goodAwayElo ? styles.teamEloGood : styles.teamEloBad}`}>
             {awayElo}
           </p>
-          {gameData.homeB2b != 0 && <p className={styles.b2bBadge}>B2B</p>}
+          {gameData.awayB2b != 0 && <p className={styles.b2bBadge}>B2B</p>}
         </div>
         <div className={`${styles.matchBox} ${styles.matchBoxLeftRounded}`}>
           <p className={styles.winPercentage}>{`${Math.round(gameData.awayWinProb * 100)}%`}</p>
